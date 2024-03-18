5a37a0f95d5bce851ab05981499d4164775ea812
Hillary Yeraldy Carpio Morales <yeraldycarpio_morales@hotmail.com>
2024-03-17 20:23:27 -0600

    Agregar archivos de proyecto.

---
+++ HYCM17032024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HYCM17032024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
